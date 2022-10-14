374c6b91da3a795e6ee3f13550bc8ead7604560c
Apple <apple@ShinkenoMacBook.local>
2022-10-14 21:24:33 +0900

    showまで設定完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:name, :email, :age, :introduction))
+    if @user.save
+      flash[:notice] = "ユーザーを新規登録しました"
+      redirect_to :users
+    else
+      render "new"
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'lessons/hello', to: 'lessons#hello'
+  get 'lessons/call', to: 'lessons#call'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
 end
